869035b32cb866df949ac8922356f4c9ef4264ec
suhong <zaqxsw327@naver.com>
2023-10-31 17:41:28 +0900

    깃허브 연결

---
+++ src/test/java/com/example/mentoring_project/MentoringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentoring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
