a8bdc8518d0026710eae3ba25b0d512f28f1de49
test <test@test.com>
2023-11-13 17:40:05 +0900

    progrow_project.board

--- src/main/java/com/example/mentoring_project/domain/BoardVO.java
+++ src/main/java/com/example/mentoring_project/domain/BoardVO.java
@@ -2,16 +2,14 @@ package com.example.mentoring_project.domain;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity
 public class BoardVO {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/mentoring_project/repository/BoardRepository.java
+++ src/main/java/com/example/mentoring_project/repository/BoardRepository.java
@@ -1,6 +1,10 @@
 package com.example.mentoring_project.repository;
 
 
+import com.example.mentoring_project.domain.BoardVO;
+
+import java.util.Optional;
 
 public interface BoardRepository{
+    Optional<BoardVO> findById(Long boardNo);
 }
